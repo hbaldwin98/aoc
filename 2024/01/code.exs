02a1e2c89ca7536362b8c9f96adf8e0c32a13f25
Hunter Baldwin <huntercbaldwin98@gmail.com>
2024-12-02 20:46:05 -0800

    Day 1 completed

--- 2024/01/code.exs
+++ 2024/01/code.exs
@@ -33,12 +33,40 @@ defmodule Part1 do
   end
 end
 
-case File.read("part1.input") do
+# Let's actually do some Elixir learning here...
+defmodule Part2 do
+  def count_and_multiply_matches(input) do
+    counts =
+      Enum.reduce(input, %{}, fn
+        # For my sanity:
+        #    Map update -> We initialize the map above with %{}
+        #    We match only on a list with a second value and update the accumulator with an initial value of 1
+        #    or the value, using an anonymous function, + 1
+        [_, second], acc -> Map.update(acc, second, 1, &(&1 + 1))
+        # this is technically unnecessary since we know the exact input
+        _, acc -> acc
+      end)
+
+    # Map.get gets a value from a map (counts) by a key (first) and defaults the value to 0 if not found 
+    Enum.map(input, fn [first | _] -> String.to_integer(first) * Map.get(counts, first, 0) end)
+    |> Enum.sum()
+  end
+end
+
+case File.read("puzzle.input") do
   {:ok, content} ->
+    IO.puts("***** Part 1 *****")
+
     content
     |> Lists.split_lines_into_groups_by_spaces()
     |> Part1.furthest_between_left_in_two_lists()
-    # |> Enum.take(1)
+    |> IO.inspect()
+
+    IO.puts("***** Part 2 *****")
+
+    content
+    |> Lists.split_lines_into_groups_by_spaces()
+    |> Part2.count_and_multiply_matches()
     |> IO.inspect()
 
   {:error, reason} ->
