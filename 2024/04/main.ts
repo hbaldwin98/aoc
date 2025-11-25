baf13a30260d41fc6993107633ce9a3a30834975
Hunter Baldwin <huntercbaldwin98@gmail.com>
2025-11-25 07:01:25 -0800

    Day 4: X-Mas

--- 2024/04/main.ts
+++ 2024/04/main.ts
@@ -44,82 +44,127 @@ data = data.replace(/\n/g, '');
 
 let count = 0;
 
+// COUNT THE X-MAS
 for (let i = 0; i < data.length; i++) {
     let char = data[i];
     let x = X(i, width);
     let y = Y(i, width);
 
-    // Left
-    if (char === 'X' &&
-        S(data, x - 1, y, width) === 'M' &&
-        S(data, x - 2, y, width) === 'A' &&
-        S(data, x - 3, y, width) === 'S'
-    ) {
-        count++;
-    }
-
-    // Right
-    if (char === 'X' &&
-        S(data, x + 1, y, width) === 'M' &&
-        S(data, x + 2, y, width) === 'A' &&
-        S(data, x + 3, y, width) === 'S'
-    ) {
-        count++;
-    }
-
-    // Up
-    if (char === 'X' &&
-        S(data, x, y - 1, width) === 'M' &&
-        S(data, x, y - 2, width) === 'A' &&
-        S(data, x, y - 3, width) === 'S'
-    ) {
-        count++;
-    }
-
-    // Down
-    if (char === 'X' &&
-        S(data, x, y + 1, width) === 'M' &&
-        S(data, x, y + 2, width) === 'A' &&
-        S(data, x, y + 3, width) === 'S'
-    ) {
-        count++;
-    }
-
-    // Towards Bottom Right
-    if (char === 'X' &&
-        S(data, x + 1, y + 1, width) === 'M' &&
-        S(data, x + 2, y + 2, width) === 'A' &&
-        S(data, x + 3, y + 3, width) === 'S'
-    ) {
-        count++;
-    }
-
-    // Towards the Top Left
-    if (char === 'X' &&
+    // Find X
+    if (char === 'A' &&
         S(data, x - 1, y - 1, width) === 'M' &&
-        S(data, x - 2, y - 2, width) === 'A' &&
-        S(data, x - 3, y - 3, width) === 'S'
-    ) {
-        count++;
-    }
-
-    // Towards the Bottom Left
-    if (char === 'X' &&
-        S(data, x - 1, y + 1, width) === 'M' &&
-        S(data, x - 2, y + 2, width) === 'A' &&
-        S(data, x - 3, y + 3, width) === 'S'
-    ) {
-        count++;
-    }
-
-    // Towards the Top Right
-    if (char === 'X' &&
         S(data, x + 1, y - 1, width) === 'M' &&
-        S(data, x + 2, y - 2, width) === 'A' &&
-        S(data, x + 3, y - 3, width) === 'S'
+        S(data, x - 1, y + 1, width) === 'S' &&
+        S(data, x + 1, y + 1, width) === 'S'
+    ) {
+        count++;
+    }
+
+    if (char === 'A' &&
+        S(data, x - 1, y - 1, width) === 'S' &&
+        S(data, x + 1, y - 1, width) === 'S' &&
+        S(data, x - 1, y + 1, width) === 'M' &&
+        S(data, x + 1, y + 1, width) === 'M'
+    ) {
+        count++;
+    }
+
+    if (char === 'A' &&
+        S(data, x - 1, y - 1, width) === 'M' &&
+        S(data, x + 1, y - 1, width) === 'S' &&
+        S(data, x - 1, y + 1, width) === 'M' &&
+        S(data, x + 1, y + 1, width) === 'S'
+    ) {
+        count++;
+    }
+
+    if (char === 'A' &&
+        S(data, x - 1, y - 1, width) === 'S' &&
+        S(data, x + 1, y - 1, width) === 'M' &&
+        S(data, x - 1, y + 1, width) === 'S' &&
+        S(data, x + 1, y + 1, width) === 'M'
     ) {
         count++;
     }
 }
+
+// COUNT THE Xmas
+//for (let i = 0; i < data.length; i++) {
+//    let char = data[i];
+//    let x = X(i, width);
+//    let y = Y(i, width);
+//
+//    // Left
+//    if (char === 'X' &&
+//        S(data, x - 1, y, width) === 'M' &&
+//        S(data, x - 2, y, width) === 'A' &&
+//        S(data, x - 3, y, width) === 'S'
+//    ) {
+//        count++;
+//    }
+//
+//    // Right
+//    if (char === 'X' &&
+//        S(data, x + 1, y, width) === 'M' &&
+//        S(data, x + 2, y, width) === 'A' &&
+//        S(data, x + 3, y, width) === 'S'
+//    ) {
+//        count++;
+//    }
+//
+//    // Up
+//    if (char === 'X' &&
+//        S(data, x, y - 1, width) === 'M' &&
+//        S(data, x, y - 2, width) === 'A' &&
+//        S(data, x, y - 3, width) === 'S'
+//    ) {
+//        count++;
+//    }
+//
+//    // Down
+//    if (char === 'X' &&
+//        S(data, x, y + 1, width) === 'M' &&
+//        S(data, x, y + 2, width) === 'A' &&
+//        S(data, x, y + 3, width) === 'S'
+//    ) {
+//        count++;
+//    }
+//
+//    // Towards Bottom Right
+//    if (char === 'X' &&
+//        S(data, x + 1, y + 1, width) === 'M' &&
+//        S(data, x + 2, y + 2, width) === 'A' &&
+//        S(data, x + 3, y + 3, width) === 'S'
+//    ) {
+//        count++;
+//    }
+//
+//    // Towards the Top Left
+//    if (char === 'X' &&
+//        S(data, x - 1, y - 1, width) === 'M' &&
+//        S(data, x - 2, y - 2, width) === 'A' &&
+//        S(data, x - 3, y - 3, width) === 'S'
+//    ) {
+//        count++;
+//    }
+//
+//    // Towards the Bottom Left
+//    if (char === 'X' &&
+//        S(data, x - 1, y + 1, width) === 'M' &&
+//        S(data, x - 2, y + 2, width) === 'A' &&
+//        S(data, x - 3, y + 3, width) === 'S'
+//    ) {
+//        count++;
+//    }
+//
+//    // Towards the Top Right
+//    if (char === 'X' &&
+//        S(data, x + 1, y - 1, width) === 'M' &&
+//        S(data, x + 2, y - 2, width) === 'A' &&
+//        S(data, x + 3, y - 3, width) === 'S'
+//    ) {
+//        count++;
+//    }
+//}
 
 console.log("FOUND:", count);
