cef730858096f196e0b9ad2ac266fdcc7759fe10
Hunter Baldwin <huntercbaldwin98@gmail.com>
2025-11-28 06:52:09 -0800

    2015 - 02 - Part 2

--- 2015/02/main.ts
+++ 2015/02/main.ts
@@ -5,6 +5,7 @@ const file = await getInput();
 let dimensionsArr = file.split('\r\n');
 let total = 0;
 
+// Packing paper
 for (const dimensions of dimensionsArr) {
     if (!dimensions?.length) continue;
 
@@ -14,4 +15,21 @@ for (const dimensions of dimensionsArr) {
     total += 2 * (a1 + a2 + a3) + Math.min(a1, a2, a3);
 }
 
-console.log(total);
+console.log('Part 1', total);
+
+let total2 = 0;
+for (const dimensions of dimensionsArr) {
+    if (!dimensions) continue;
+
+    let [l, w, h] = dimensions.split('x').map(Number);
+    if (!l || !w || !h) continue;
+    const sides = [l, w, h].toSorted((a, b) => a - b);
+
+    let bow = (l * w * h);
+    let wrap = 2 * (sides[0]! + sides[1]!);
+    total2 += bow + wrap;
+}
+
+console.log("Part 2", total2);
+
+
