a3bfd765e966a75ff5bd8d0e0f75418b09249c2d
Hunter Baldwin <huntercbaldwin98@gmail.com>
2025-12-01 06:24:58 -0800

    2025 - 01 - Part 2

--- 2025/01/main.ts
+++ 2025/01/main.ts
@@ -10,7 +10,7 @@ for (const line of getLines(input)) {
     const distance = Number(line.slice(1, line.length));
     const direction = rotation === 'R' ? 1 : -1;
 
-    current = (current + (direction * distance)) % 100; 
+    current = (current + (direction * distance)) % 100;
 
     if (current === 0) {
         password++;
@@ -18,3 +18,22 @@ for (const line of getLines(input)) {
 }
 
 console.log('Part 1: Password', password);
+
+password = 0;
+current = 50;
+
+for (const line of getLines(input)) {
+    const rotation = line[0];
+    const distance = Number(line.slice(1, line.length));
+
+    let direction = rotation === 'R' ? 1 : -1;
+    for (let i = 0; i < distance; i++) {
+        current = (current + direction) % 100;
+
+        if (current === 0) {
+            password++;
+        }
+    }
+}
+
+console.log('Part 2: Password', password);
