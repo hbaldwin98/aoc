2d6f09be6edeab5856314869139944b6ceef46c6
Hunter Baldwin <huntercbaldwin98@gmail.com>
2025-12-04 05:49:46 -0800

    2025 - 03 - Part 2

--- 2025/03/main.ts
+++ 2025/03/main.ts
@@ -28,9 +28,39 @@ for (const bank of banks) {
         }
     }
 
-    console.log(first, second, bank);
     voltage += Number(`${first}${second}`);
 }
 
 
 console.log('Part 1', voltage);
+
+voltage = 0;
+for (const bank of banks) {
+    const TARGET_LENGTH = 12;
+    const stack: number[] = [];
+
+    let remaining = bank.length - TARGET_LENGTH;
+
+    for (let i = 0; i < bank.length; i++) {
+        const num = +bank[i]!;
+        while (
+            remaining > 0 &&
+            stack.length > 0 &&
+            stack[stack.length - 1]! < num
+        ) {
+            stack.pop();
+            remaining--;
+        }
+
+        stack.push(num);
+    }
+
+    while (stack.length > TARGET_LENGTH) {
+        stack.pop();
+    }
+
+    console.log(stack.join(''));
+    voltage += Number(stack.join(''));
+}
+
+console.log('Part 2', voltage);
